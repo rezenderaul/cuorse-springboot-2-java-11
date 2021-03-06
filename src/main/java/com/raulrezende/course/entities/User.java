2141bae55a810ac6138668529f1455baf0aa04d6
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-03-06 12:23:41 -0300

    H2 database, test profile, JPA

--- src/main/java/com/raulrezende/course/entities/User.java
+++ src/main/java/com/raulrezende/course/entities/User.java
@@ -2,9 +2,17 @@ package com.raulrezende.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // Mapeamento pelo JPA
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id // Informa ao spring qual é a chave primária da tabela no banco de dados
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // informa que o id é auto incrementável
 	private Long id;
 	private String name;
 	private String email;
