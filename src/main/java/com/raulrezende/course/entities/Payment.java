9653c3ffa04e14292e8fd69179539e04567ad8f7
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-03-07 15:40:11 -0300

    Subtotal & Total methods

--- src/main/java/com/raulrezende/course/entities/Payment.java
+++ src/main/java/com/raulrezende/course/entities/Payment.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name="tb_payment") // Nome dado a tabela no banco de dados
 public class Payment implements Serializable {
@@ -21,6 +23,7 @@ public class Payment implements Serializable {
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne // Um para um
 	@MapsId // Inserido na classe dependente
 	private Order order;
