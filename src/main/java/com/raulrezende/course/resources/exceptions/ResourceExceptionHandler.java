2e0e57b9695c2a5083574f27b4b7df96f9279ff7
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-03-07 17:35:42 -0300

    Exception handling - finfById

---
+++ src/main/java/com/raulrezende/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.raulrezende.course.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.raulrezende.course.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice // Intercepta as exceções
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class) // Intercepta qualtqer exceção do tipo informado
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/raulrezende/course/services/UserService.java
+++ src/main/java/com/raulrezende/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.raulrezende.course.entities.User;
 import com.raulrezende.course.repositories.UserRepository;
+import com.raulrezende.course.services.exceptions.ResourceNotFoundException;
 
 @Service // Registra a classe como serviço do spring, podendo assim ser injetado automáticamente
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		// A operação get do Optional retorna, um objeto do tipo declarado
 	}
 	
