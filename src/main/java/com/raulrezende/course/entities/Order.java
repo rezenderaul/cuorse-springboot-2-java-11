6eb8c29d1e7d44cb3881998a5a3befb939358bc2
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-03-07 15:23:57 -0300

    Payment one-to-one association

--- src/main/java/com/raulrezende/course/entities/Order.java
+++ src/main/java/com/raulrezende/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -38,6 +40,10 @@ public class Order implements Serializable {
 	// id vem da classe OrderItem, da variável OrderItemPK id
 	// order vem da classe OrderItemPK, da variável Order order
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy="order", cascade = CascadeType.ALL) 
+	// vem classe Payment, da variável Order order
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -83,6 +89,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
